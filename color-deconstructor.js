750b424ffd05e878bda64090706353e3c26edd77
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-06-17 10:58:30 -0400

    .

--- color-deconstructor.js
+++ color-deconstructor.js
@@ -3,18 +3,15 @@
 
 function colorDeconstructor(secondary){
 
-    let dOrange = secondary === 'orange';
-    let dPurple = secondary === 'purple';
-    let dGreen = secondary === 'green';
-
-        if (dOrange) {
-            return 'Orange is made by combining red and yellow!'
-        } else if (dPurple) {
-            return 'Purple is made by combining red and blue!'
-        } else if (dGreen) {
-            return 'Green is made by combining blue and yellow!'
-        } 
-        else{return "Sorry, that's not a secondary color!"
+  if (secondary === 'orange') {
+      return 'Orange is made by combining red and yellow!'
+    } else if (secondary === 'purple') {
+       return 'Purple is made by combining red and blue!'
+    } else if (secondary === 'green') {
+        return 'Green is made by combining blue and yellow!'
+    } 
+    else {
+        return "Sorry, that's not a secondary color!"
     }
 }
 
